71a1ac70b99d486b554f45bf29958ac674e4d954
Claire Davies <claire.davies@junitech.com>
2021-05-21 12:46:40 +0100

    created wrapper and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import Header from "./components/Header";
 import library from "./images/book-library.jpg"
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
@@ -14,10 +16,12 @@ function App() {
 
       <Navbar/>
       <Header backgroundImage={library}/>
+      <Wrapper>
       <Route exact path="/" component={Search} />
       <Route exact path="/search" component={Search} />
       <Route exact path="/saved" component={Saved} />
-                        
+      </Wrapper>   
+      <Footer />              
     </div>
     </Router>
   );
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footerStyle.css";
+
+function Footer() {
+    return (
+
+
+        <footer className="footer"><span className="copyright">© 2021 All Rights Reserved</span></footer>
+
+    );
+}
+
+export default Footer;
